8f44d30f074c1ca23a3410a7dae5d5d01e319f18
Aifuna <aifunashuhaib@gmail.com>
2025-12-07 23:03:08 +0530

    Update CustomerLogin.jsx

--- frontend/src/pages/CustomerLogin.jsx
+++ frontend/src/pages/CustomerLogin.jsx
@@ -10,7 +10,7 @@ export default function CustomerLogin() {
     e.preventDefault();
 
     try {
-      const res = await fetch("https://mern-ecommerce-project-igh5.onrender.com/api/users/login", {
+      const res = await fetch("http://localhost:5000/api/users/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, password }),
